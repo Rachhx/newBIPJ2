1e2a1bb6b5ac38a1a7124fc2212bf41874f964c0
Jenevieve <jenevieveeee@gmail.com>
2020-01-29 13:05:57 +0800

    jens modal

---
+++ src/app/modals/cart-modal/cart-modal.page.ts
@@ -0,0 +1,61 @@
+import { Product, CartService } from './../../services/cart.service';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
+import { CrudService } from 'src/app/crud.service';
+ 
+@Component({
+  selector: 'app-cart-modal',
+  templateUrl: './cart-modal.page.html',
+  styleUrls: ['./cart-modal.page.scss'],
+})
+export class CartModalPage implements OnInit {
+  PName: string;
+  PPrice: string;
+ 
+  cart: Product[] = [];
+ 
+  constructor(private cartService: CartService, private modalCtrl: ModalController, private alertCtrl: AlertController, private crudService: CrudService) { }
+ 
+  ngOnInit() {
+    this.cart = this.cartService.getCart();
+  }
+ 
+  decreaseCartItem(product) {
+    this.cartService.decreaseProduct(product);
+  }
+ 
+  increaseCartItem(product) {
+    this.cartService.addProduct(product);
+  }
+ 
+  removeCartItem(product) {
+    this.cartService.removeProduct(product);
+  }
+ 
+  getTotal() {
+    return this.cart.reduce((i, j) => i + j.price * j.amount, 0);
+  }
+ 
+  close() {
+    this.modalCtrl.dismiss();
+  }
+
+  async checkout() {
+  console.log(this.cart) 
+  }
+
+CreateRecord() {
+  let record = {};
+  record['PName'] = this.PName;
+  record['PPrice'] = this.PPrice;
+  record['User'] = "12345abc";
+
+  this.crudService.create_NewCart(record).then(resp => {
+   console.log(this.cart) 
+    console.log(resp);
+  })
+    .catch(error => {
+      console.log(error);
+    });
+}
+}
