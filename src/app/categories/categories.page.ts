3563a2ea9bf74be1ba038962f08076e82144c3e4
Jenevieve <jenevieveeee@gmail.com>
2020-01-29 12:30:31 +0800

    jen

---
+++ src/app/categories/categories.page.ts
@@ -0,0 +1,75 @@
+import { CartService } from './../services/cart.service';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CartModalPage } from '../pages/cart-modal/cart-modal.page';
+import { BehaviorSubject } from 'rxjs';
+import { CrudService } from '../crud.service';
+import { Product } from '../shared/product';
+ 
+@Component({
+  selector: 'app-categories',
+  templateUrl: 'categories.page.html',
+  styleUrls: ['categories.page.scss']
+})
+export class CategoriesPage {
+  PName: string;
+  PPrice: string;
+  cart = [];
+  products = [];
+  cartItemCount: BehaviorSubject<number>;
+ 
+  @ViewChild('cart', {static: false, read: ElementRef})fab: ElementRef;
+ 
+  constructor(private cartService: CartService, private modalCtrl: ModalController,private crudService: CrudService) {}
+ 
+  ngOnInit() {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+  }
+ 
+  addToCart(product : Product) {
+  let record = {};
+  record['PName'] = product.name;
+  record['PPrice'] = product.price;
+  record['user'] = "12345abc";
+
+  this.crudService.create_NewCart(record).then(resp => {
+   console.log(this.cart) 
+    console.log(resp);
+  })
+    .catch(error => {
+      console.log(error);
+    });
+    this.cartService.addProduct(product);
+    this.animateCSS('tada');
+  }
+ 
+  async openCart() {
+    this.animateCSS('bounceOutLeft', true);
+ 
+    let modal = await this.modalCtrl.create({
+      component: CartModalPage,
+      cssClass: 'cart-modal'
+    });
+    modal.onWillDismiss().then(() => {
+      this.fab.nativeElement.classList.remove('animated', 'bounceOutLeft')
+      this.animateCSS('bounceInLeft');
+    });
+    modal.present();
+  }
+ 
+  animateCSS(animationName, keepAnimated = false) {
+    const node = this.fab.nativeElement;
+    node.classList.add('animated', animationName)
+    
+    //https://github.com/daneden/animate.css
+    function handleAnimationEnd() {
+      if (!keepAnimated) {
+        node.classList.remove('animated', animationName);
+      }
+      node.removeEventListener('animationend', handleAnimationEnd)
+    }
+    node.addEventListener('animationend', handleAnimationEnd)
+  }
+}
